5ea510bbdaee2db22068829d8f9fcd8c016c5d86
yoavuzan <76042880+yoavuzan@users.noreply.github.com>
2023-05-17 12:23:41 +0300

    Delete src directory

--- src/Bishop.cpp
+++
@@ -1,8 +0,0 @@
-#include "Bishop.h"
-char Bishop::GetPeice()
-{
-	if (GetColor() == Color::White)
-		return 'B';
-	else
-		return 'b';
-}
--- src/King.cpp
+++
@@ -1,9 +0,0 @@
-#include "King.h"
-char King::GetPeice()
-{
-	if (GetColor() == Color::White)
-		return 'K';
-	else
-		return 'k';
-}
-
